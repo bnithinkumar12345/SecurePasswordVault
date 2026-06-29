5d4a208dd433b0b951a78dac5a5da02283fa5f66
B Nithin Kumar <bnithinkumar12345@gmail.com>
2026-06-29 10:10:34 +0530

    Initial project setup

---
+++ src/main/java/com/nithin/Main.java
@@ -0,0 +1,13 @@
+package com.nithin;
+
+import com.nithin.config.DBConnection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DBConnection.getConnection();
+
+    }
+
+}
---
+++ src/main/java/config/DBConnection.java
@@ -0,0 +1,34 @@
+package com.nithin.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/password_vault";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Nithin@123";
+
+    public static Connection getConnection() {
+
+        try {
+
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            System.out.println("Database Connected Successfully!");
+
+            return connection;
+
+        } catch (SQLException e) {
+
+            System.out.println("Database Connection Failed!");
+
+            e.printStackTrace();
+
+            return null;
+        }
+
+    }
+
+}
